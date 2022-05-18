eef47d43f4439cf47187f61c0327bbcb7e8e51d5
Anabel <anabel.batista@lehman.cuny.edu>
2022-05-17 20:35:22 -0400

    final commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,31 +3,52 @@ import React, { Component } from 'react'
 class Form extends Component {
   constructor(props) {
     super(props)
-
-    /*
-            TODO - set initial state for link name and URL 
-        */
+    // link name and URL
+    this.state = {name: "" , URL: "" }
   }
 
-  handleChange = (event) => {
-    /*
-            TODO - Logic for changing state based on form changes
-        */
+  handleNameChange = (event) => {
+    this.setState({
+    name: event.target.value 
+    })
   }
+  handleURLChange = (event) => {
+    this.setState({
+    URL: event.target.value
+
+    })
+  }
+
 
   onFormSubmit = (event) => {
-    // to prevent page reload on form submit
-    event.preventDefault()
-
-    /*
-            TODO - Logic for calling props to handle submission and setting state changes
-        */
+    event.preventDefault();
+    const submission = {
+      name: this.state.name,
+      URL: this.state.URL
+    }
+    this.props.handleSubmit(submission);
+    this.setState({name: "", URL: ""})
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.onFormSubmit}>
         {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+        <label for="name">Name</label>
+        <input
+          type = "text"
+          onChange={this.handleNameChange}
+          value={this.state.name}
+          />
+          <label for="URL">URL</label>
+          <input
+          type = "text"
+          onChange={this.handleURLChange}
+          value={this.state.URL}
+          />
+
+            <button type="submit"> Submit</button>
+
       </form>
     )
   }
--- src/components/Table.js
+++ src/components/Table.js
@@ -34,9 +34,15 @@ const TableBody = (props) => {
 }
 
 const Table = (props) => {
-  {
-    /*TODO - return <table> component, TableHeader and TableBody  and pass props!*/
-  }
+  
+return (
+    <table>
+      <TableHeader />
+      <TableBody linkData = {props.linkData} 
+      removeLink = {props.removeLink} />
+    </table>
+   )
+  
 }
 
 export default Table
